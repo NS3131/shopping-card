bf17e378da5ecfd03618e6f8f17cfe92f4f74e11
NS3131 <shaumba@hotmail.com>
2025-07-26 18:33:02 +0100

    a

---
+++ script.js
@@ -0,0 +1,8 @@
+// we need to select all the necessary elements
+// under the articles add,minus,delete,like,total unit per price,total display, quantities
+
+
+const plusBtns = document.querySelectorAll(".fa-plus-circle");
+const minusBtns = document.querySelectorAll(".fa-minus-circle");
+const deleteBtns = document.querySelectorAll(".fa-trash-alt");
+
